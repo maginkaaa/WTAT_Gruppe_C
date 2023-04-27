303eb422e520ec49c35ad0f88ae92f2fde7782c2
Christian Kaltenbach <christian123kalt@gmail.com>
2023-04-27 20:01:12 +0200

    sprint02

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const profileController = require("./controllers/profileController");
+const signupController = require("./controllers/signupController");
 const port = 3000,
  express = require("express"),
  app = express();
+ app.set("views", (__dirname + "/views")),
+ app.set("view engine", "ejs"),
+
+
  app.get("/profile/:username", profileController.sendReqParam);
+ app.get("/sign_up", signupController.getSignUp);
 app.listen(port, () => {
  console.log(`Server running on port: ${port}`);
 });
