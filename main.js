e2f58a268c83c9c34cbe0972dc9dc0ad100d1cc4
Larry <104570219+Larry-Master@users.noreply.github.com>
2023-05-26 21:10:01 +0200

    Mongo data transfer (#22)
    * First Post
    
    * SignUp
    
    * Finished Posting Data
    
    * updated user.ejs
    
    ---------
    
    Co-authored-by: Magdalena <57997851+maginkaaa@users.noreply.github.com>

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const signupController = require("./controllers/signupController");
 const loginController = require("./controllers/loginController");
 const jobAddController = require("./controllers/jobAddController");
 const jobDetailController = require("./controllers/jobDetailController");
-const User = require("./models/user.module");
 const jobOpening = require("./models/jobOpening.module");
 
 const mongoose = require("mongoose");
@@ -12,8 +11,15 @@ mongoose.connect(
  {useNewUrlParser: true}
 );
 
+const db = mongoose.connection;
+db.once("open", () => {
+console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
+
 const port = 3000,
 express = require("express"),
+
 app = express();
 app.use(express.urlencoded({ extended: true })),
 app.set("views", (__dirname + "/views")),
@@ -21,11 +27,13 @@ app.set("view engine", "ejs"),
 
 app.get("/profile/:username", profileController.sendReqParam);
 app.get("/signup", signupController.getSignUp);
+app.get("/user", signupController.getAllUser);
 app.get("/login", loginController.getLogIn);
-
 app.get("/jobs/:id", jobDetailController.getJobInfo);
 app.get("/job/add", jobAddController.addJob);
+
 app.post("/job/add", jobAddController.saveJob);
+app.post("/signup", signupController.saveUser);
 
 app.listen(port, () => {
  console.log(`Server running on port: ${port}`);
