1e769436010763cf064c32a9d8da7aef654f8bfe
Larry <104570219+Larry-Master@users.noreply.github.com>
2023-06-30 16:27:57 +0200

    Removed old code

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,43 +30,6 @@ exports.getSignUp = (req, res) => {
   res.render("signUp");
 };
 
-/*exports.saveUser = (req, res, next) => {
-  let newUser;
-  let users = [];
-  bcrypt.hash(req.body.password, 10).then(hash => {
-    newUser = new User({
-      username: req.body.username,
-      password: hash,
-  });
-  users.push(newUser);
-  newUser.save()
-
-  .then((newUser) => {
-    req.flash("success", `${newUser.fullName}'s account created successfully!`);
-    res.locals.redirect = "/users";
-    res.locals.user = newUser;
-    next();
-  })
-  .then(() => {
-    return User.find({}); // Retrieve the updated list of users
-  })
-  .then(() => {
-    res.render("users", { users });
-  })
-  .catch((error) => {
-    console.log(`Error saving user: ${error.message}`);
-    req.flash("error", `Failed to create user account because: ${error.message}.`);
-    res.locals.redirect = "/users";
-    next();
-  });
-
-  })
-  .then(() => {
-    res.render("users", { users });
-  })
-  .catch(error => { console.log(`Error in hashing password: ${error.message}`);
-  next(error);    });
-};*/
 
 exports.saveUser = async (req, res) => {
   try {
@@ -87,38 +50,6 @@ exports.saveUser = async (req, res) => {
   }
 };
 
-
-/*exports.authenticate = (req, res, next) => {
-    User.findOne({email: req.body.email})
-     .then(user => {
-       if (user) {
-        bcrypt.compare(req.body.password, user.password)
-          .then(passwordsMatch => {
-             if (passwordsMatch) {
-                        res.locals.redirect = `/users/${user._id}`;
-                        req.flash("success", `${user.username}'s logged in ➥successfully!`);
-                        res.locals.user = user;
-                      }
-                      else
-                      {
-                          req.flash("error", "Failed to log in user account: ➥Incorrect Password.");
-                          res.locals.redirect = "/users/login";
-                        }
-                        next();
-                      });
-                    }
-                    else
-                    {
-                      req.flash("error", "Failed to log in user account: User ➥account not found.");
-                      res.locals.redirect = "/users/login";
-                      next();
-                    }
-                  })
-                  .catch(error => {
-                    console.log(`Error logging in user: ${error.message}`);
-                    next(error);
-                  });
-                }*/
 
 exports.authenticate = passport.authenticate("local", {
     failureRedirect: "/login",
