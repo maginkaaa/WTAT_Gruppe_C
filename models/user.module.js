f1cda9e5533698655a7e4c5b8095a6d1de643a3e
nellehrl <104624167+nellehrl@users.noreply.github.com>
2023-07-07 11:31:55 +0200

    API token generation + jwt

--- models/user.module.js
+++ models/user.module.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
+const randToken = require
+("rand-token");
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   //password: { type: String, required: true }
+});
+userSchema.pre("save", function(next) {
+  let user = this;
+  if (!user.apiToken) user.apiToken =
+      randToken.generate(16);
+  next();
 });
 
 userSchema.plugin(passportLocalMongoose, {
