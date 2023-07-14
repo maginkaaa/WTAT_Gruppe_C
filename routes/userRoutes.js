176ae43e7073942825fe63004a39bf2be0a815d8
nellehrl <104624167+nellehrl@users.noreply.github.com>
2023-07-14 20:53:08 +0200

    API token generation + jwt (#44)
    * API token generation + jwt
    
    * Fixxed Signup + Added Token
    
    * SignUp Fix
    
    ---------
    
    Co-authored-by: Larry <104570219+Larry-Master@users.noreply.github.com>

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,16 +3,18 @@ userController = require("../controllers/userController");
 
 router.delete("/user/:id/delete", userController.deleteUser);
 
-
-router.get("/signup", userController.getSignUp);
-router.get("/user", userController.getAllUser);
 router.get("/login", userController.getLogIn);
+router.get("/user", userController.getAllUser);
 router.get("/signup", userController.addUser);
 router.get("/:username", userController.sendReqParam);
+router.post("/signup", userController.saveUser);
+
+router.use(userController.verifyJWT);
+
 
 router.put("/user/:id/update", userController.updateUser);
 
-router.post("/signup", userController.saveUser);
-router.post("/login", userController.authenticate, userController.redirectAfterLogin);
+
+router.post("/login", userController.apiAuthenticate, userController.redirectAfterLogin);
 
 module.exports = router;
